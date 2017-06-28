2da94c0a7ac5fb87c7852ab354155d44c3adf114
jacktang <jack at zzfs>
2017-06-28 15:21:50 +0800

    1

--- radix.c
+++ radix.c
@@ -5,55 +5,63 @@
 #include "comm.h"
 #include "radix-tree.h"
 
-static uint64_t buf[MAX_TEST_NUM];
-RADIX_TREE(root);
+//static uint64_t buf[MAX_TEST_NUM];
+struct radix_data
+{
+	uint64_t index;
+	uint64_t data;
+};
 
-void find_entry(uint64_t entry, int num)
+static struct radix_data radix_data[MAX_TEST_NUM];
+
+void find_entry(struct radix_tree_root *root, uint64_t entry)
+{
+	/* for (int i = 0; i < num; ++i) */
+	/* { */
+	/* 	if (buf[i] == entry) */
+	/* 		return; */
+	/* } */
+	struct radix_data *data = radix_tree_lookup(root, entry);
+	printf("entry %lu, index = %lu, data %lu\n", entry, data->index, data->data);
+//	assert(0);
+}
+
+void delete_entry(struct radix_tree_root *root, uint64_t entry)
+{
+/* 	for (int i = 0; i < num; ++i) */
+/* 	{ */
+/* 		if (buf[i] == entry) */
+/* 		{ */
+/* 			buf[i] = buf[num - 1]; */
+/* //			memmove(&buf[i], &buf[i+1], num - i - 1); */
+/* 			return; */
+/* 		} */
+/* 	} */
+//	assert(0);
+}
+
+void find(struct radix_tree_root *root, uint64_t *data, int num)
 {
 	for (int i = 0; i < num; ++i)
 	{
-		if (buf[i] == entry)
-			return;
+		find_entry(root, data[i]);
 	}
-	assert(0);
 }
 
-void delete_entry(uint64_t entry, int num)
+void delete(struct radix_tree_root *root, uint64_t *data, int num)
 {
 	for (int i = 0; i < num; ++i)
 	{
-		if (buf[i] == entry)
-		{
-			buf[i] = buf[num - 1];
-//			memmove(&buf[i], &buf[i+1], num - i - 1);
-			return;
-		}
-	}
-	assert(0);
-}
-
-void find(uint64_t *data, int num)
-{
-	for (int i = 0; i < num; ++i)
-	{
-		find_entry(data[i], num);
+		delete_entry(root, data[i]);
 	}
 }
 
-void delete(uint64_t *data, int num)
-{
-	for (int i = 0; i < num; ++i)
-	{
-		delete_entry(data[i], num);
-	}
-}
-
-void insert(uint64_t *data, int num)
+void insert(struct radix_tree_root *root, uint64_t *data, int num)
 {
 	for (int i = 0; i < num; ++i)
 	{
 //		buf[i] = data[i];
-		int ret = radix_tree_insert(&root, data[i], &data[i]);
+		int ret = radix_tree_insert(root, data[i], &radix_data[i]);
 		assert(ret == 0);
 	}
 }
@@ -66,10 +74,27 @@ int main(int argc, char *argv[])
 	uint64_t *data_r = read_rand_num(num);
 	assert(data_r);
 
+	radix_tree_init();
+	RADIX_TREE(root);
+
+	for (int i = 0; i < num; ++i)
+	{
+		radix_data[i].index = i;
+		radix_data[i].data = data[i];
+	}
+
+//	uint64_t n = 100;
+	radix_tree_insert(&root, 1, 0x300);
+	void *t = radix_tree_lookup(&root, 1);	
+	radix_tree_insert(&root, 1, 0x100);
+	t = radix_tree_lookup(&root, 1);	
+	radix_tree_insert(&root, 1, 0x200);
+	t = radix_tree_lookup(&root, 1);		
+
 	time_begin();
-	insert(data, num);
-	find(data_r, num);
-	delete(data_r, num);
+	insert(&root, data, num);
+	find(&root, data_r, num);
+	delete(&root, data_r, num);
 	int diff = time_diff();
 	printf("[%d] ", diff);
     return 0;
